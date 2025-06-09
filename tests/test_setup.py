e5a06738ec0b3f31a7e62c4aabf2d37e31a3d415
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2025-06-09 16:47:42 +0200

    add capgains account to test setup

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -24,6 +24,7 @@ ibflex_config = {
     "dividend_account": "Income:Investments:Dividend:IB:{currency}:{symbol}",
     "dividend_payee": "{symbol} distribution",
     "interest_account": "Income:Investments:Interest:IB:{symbol}",
+    "capgain_account": "Income:Investment:Capital-Gains:IB:{currency}",
     "broker_interest_account": "Income:Investments:Interest:IB:Cash",
     "fees_account": "Expenses:Commissions:IB",
     "whtax_account": "Expenses:Investments:IB:WithholdingTax",
